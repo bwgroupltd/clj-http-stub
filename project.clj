cf47f3d0cee77ada23fd1df3423f6154daf66977
Dan Galvin <dan.galvin@soundunited.com>
2025-01-16 13:33:54 +0000

    set project version up to 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject audiogum/clj-http-stub "0.0.3"
+(defproject audiogum/clj-http-stub "0.0.4-SNAPSHOT"
   :description "Helper for faking clj-http and http-kit requests in testing"
   :url "https://github.com/bwgroupltd/clj-http-stub"
   :license {:name "MIT License"
